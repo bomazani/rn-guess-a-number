6eb31414b43aae6bd7ea80615ab103a07b1e1165
Bob Hunt <bob.hunt.email@gmail.com>
2021-05-02 10:25:29 -0400

    Imported the Header component into App.js, added some styling and the title prop.

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import Header from './components/Header';
+
 export default function App() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Header tite="Guess a Number" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+  }
 });
