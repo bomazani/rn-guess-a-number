351d34ad8409a2003eee3f3ee435b6f5081fc029
Bob Hunt <bob.hunt.email@gmail.com>
2021-05-05 16:19:22 -0400

    Fixed typo in StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -28,7 +28,7 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if (chosenNumber === NaN || chosenNumber <= 0 \\ chosenNumber > 99) {
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
            return; 
         }
         setConfirmed(true);
